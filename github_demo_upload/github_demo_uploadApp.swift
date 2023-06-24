4908c8d659fc31154a17bec20b56f06dd4d2b076
didi <anahita1177@gmail.com>
2023-06-24 13:55:19 +0400

    Initial Commit

---
+++ github_demo_upload/github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demo_uploadApp.swift
+//  github_demo_upload
+//
+//  Created by Seyedeh Anahita on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
